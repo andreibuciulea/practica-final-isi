5a5ab50ff728e9f457b457a7b9206f2989a4fbf5
andreibuciulea <andreibuciulea@gmail.com>
2017-12-21 17:04:50 +0100

    añadiendo funcionalidades a Main.java

--- src/main/java/urjc/isi/practica_final_isi/Main.java
+++ src/main/java/urjc/isi/practica_final_isi/Main.java
@@ -6,12 +6,34 @@ import spark.Request;
 import spark.Response;
 
 import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
   
     public static String doWork(Request request, Response response) throws ClassNotFoundException, URISyntaxException {
-	String result = new String("Hello World");
-
+	
+    String result = request.url();
+    String[] res = result.split("/");
+    String[] res1 = result.split("%20");
+    System.out.println(res[3]); //argumento[3] = distancia o vecinos
+    
+    if(res[3].equals("vecinos")) {
+    	System.out.println("Estoy en vecinos");
+    	//a index graph se le pasa un autor y devuelve las peliculas en las que sale
+    				  //se le pasa una pelicula y devuelve los actores
+    	res[4]=res[4].replace("%20"," ");
+    	System.out.println(res[4]);
+    	
+    	
+    }else if(res[3].equals("distancia")) {
+    	System.out.println("Estoy en distancia");
+    	res[4]=res[4].replace("%20"," ");
+    	res[5]=res[5].replace("%20"," ");
+    }else{
+    	System.out.print("Error: Parámetros erroneos ");
+    	System.out.println(res[3]);
+    }
 	return result;
     }
 
@@ -19,7 +41,11 @@ public class Main {
         port(getHerokuAssignedPort());
 
         // spark server
-        get("/hello", Main::doWork);
+        get("/*", Main::doWork);
+        
+        //get("/hello/*/to/*", (request, response) -> {
+        //    return "Number of splat parameters: " + request.splat().length;
+        //});
 
     }
 
